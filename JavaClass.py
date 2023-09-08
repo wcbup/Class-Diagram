32c07fe46e335080daec751b7754ebc868a60d6f
wcbup <wcbup123@foxmail.com>
2023-09-08 15:36:31 +0200

    divide id set and name set in 'JavaClass'

--- JavaClass.py
+++ JavaClass.py
@@ -11,15 +11,29 @@ class JavaClass:
         self.id = package_name + "." + class_name
         self.package_name = package_name
         self.name = class_name
-        self.inherit_set: Set[str] = set()  # the of id of the classes it extends
-        self.realize_set: Set[str] = set()  # the of id of the classes it implement
-        self.aggregate_set: Set[
+
+        self.inherit_id_set: Set[str] = set()  # the id of the classes it extends
+        self.inherit_name_set: Set[str] = set()  # the name of the classes it extends
+
+        self.realize_id_set: Set[str] = set()  # the id of the classes it implement
+        self.realize_name_set: Set[str] = set()  # the name of the classes it implement
+
+        self.aggregate_id_set: Set[
             str
-        ] = set()  # the of id of the classes its field contains
-        self.compose_set: Set[
+        ] = set()  # the id of the classes its field contains
+        self.aggregate_name_set: Set[
             str
-        ] = set()  # the of id of the classes its nonstatic innerclass
-        self.depend_set: Set[str] = set()  # the of id of the classes it depends on
+        ] = set()  # the name of the classes its field contains
+
+        self.compose_id_set: Set[
+            str
+        ] = set()  # the id of the classes its nonstatic innerclass
+        self.compose_name_set: Set[
+            str
+        ] = set()  # the name of the classes its nonstatic innerclass
+
+        self.depend_id_set: Set[str] = set()  # the id of the classes it depends on
+        self.depend_name_set: Set[str] = set()  # the id of the classes it depends on
 
     def __hash__(self) -> int:
         return self.id.__hash__()
